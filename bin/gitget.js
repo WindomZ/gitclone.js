2d7f82b532db7cf6e5337fd784660d1a402e4257
WindomZ <git.windomz@gmail.com>
2017-06-01 08:26:37 +0800

    init command

--- bin/gitget.js
+++ bin/gitget.js
@@ -1,4 +1,54 @@
+#!/usr/bin/env node
 /**
  * Created by WindomZ on 17-6-1.
  */
 'use strict'
+
+const program = require('commander')
+
+const pkg = require('../package.json')
+
+let noArgs = true
+
+program
+  .version(pkg.version)
+  .description(pkg.description)
+  .usage('[options] <repo>')
+  .action((repo, options) => {
+    noArgs = false
+
+    console.log('repo: %j', repo)
+  })
+
+program
+  .command('list')
+  .alias('ls')
+  .action(options => {
+    noArgs = false
+
+    console.log('list')
+  })
+
+program
+  .command('search <keyword>')
+  .alias('s')
+  .action((keyword, options) => {
+    noArgs = false
+
+    console.log('search: %j', keyword)
+  })
+
+program
+  .command('link <filepath>')
+  .alias('l')
+  .action((filepath, options) => {
+    noArgs = false
+
+    console.log('link: %j', filepath)
+  })
+
+program.parse(process.argv)
+
+if (noArgs) {
+  program.outputHelp()
+}
